1b4619072ba8a336baed56a8c670d5dd03ebe5ec
Raymond Maung <raymaung@gmail.com>
2017-04-21 21:22:48 +1000

    Use phase alias instead

--- apps/loca/test/translation_service/phase_test.exs
+++ apps/loca/test/translation_service/phase_test.exs
@@ -1,10 +1,10 @@
 defmodule Loca.TranslationService.PhaseTest do
   use ExUnit.Case
 
-  # alias Loca.TranslationService.Phase
+  alias Loca.TranslationService.Phase
 
   test "attributes" do
-    p = %Loca.TranslationService.Phase{language: "english", text: "Hello"}
+    p = %Phase{language: "english", text: "Hello"}
     assert p.language == "english"
     assert p.text == "Hello"
   end
