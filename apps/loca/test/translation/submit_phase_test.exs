01ff0f04773d3beb88affc711c26c8c1fa40facc
Raymond Maung <raymaung@gmail.com>
2017-04-21 18:25:04 +1000

    Rename to submit_phase_test

--- apps/loca/test/translation/submit_phase_test.exs
+++ apps/loca/test/translation/submit_phase_test.exs
@@ -1,17 +1,17 @@
-defmodule Loca.TranslationTest do
+defmodule Loca.Translation.SubmitPhaseTest do
   use ExUnit.Case
 
   use Loca.DataCase
 
   alias Loca.Translation
 
-  test "submit phase" do
+  test "submit_phase/1" do
     assert {:ok, phase} = Translation.submit_phase(%{language: "eng", text: "hello"})
     assert phase.language == "eng"
     assert phase.text == "hello"
   end
 
-  test "submit phase - required fields" do
+  test "submit_phase/1 - required fields" do
     assert {:error, _} = Translation.submit_phase(%{language: "", text: "hello"})
     assert {:error, _} = Translation.submit_phase(%{language: "eng", text: ""})
   end
