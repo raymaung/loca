8da36cba4439068a9b1250e30e771bc93aaeddeb
Raymond Maung <raymaung@gmail.com>
2017-04-21 21:29:31 +1000

    Add Translation struct

--- apps/loca/test/translation_service/translation_test.exs
+++ apps/loca/test/translation_service/translation_test.exs
@@ -11,4 +11,16 @@ defmodule Loca.TranslationService.TranslationTest do
     assert t.text == "Bonjour"
     assert t.phase == p
   end
+
+  test "unicode support" do
+    p = %Phase{language: "english", text: "Hello"}
+    t = %Translation{language: "Chinese", text: "你好", phase: p}
+    assert t.text == "你好"
+  end
+
+  test "mixing multiple languages" do
+    p = %Phase{language: "english", text: "Hello"}
+    t = %Translation{language: "Japanese", text: "こんにちはHello", phase: p}
+    assert t.text == "こんにちはHello"
+  end
 end
